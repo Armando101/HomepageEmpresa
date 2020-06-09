e9b4e93dd1dbbe87c489e7a0e34831c49629797c
Armando <rivera.armando997@gmail.com>
2020-06-08 21:03:39 -0500

    Referenciando las imagenes por el link y modificar tamaños de fuente

--- src/app/components/home/services/cards.service.ts
+++ src/app/components/home/services/cards.service.ts
@@ -11,10 +11,10 @@ export class CardService {
 
 	constructor() {
 		this.cards = [
-			new Card('/assets/img/consultoria.jpg', 'Consultoría', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, at!', '#2f78c6'),
-			new Card('/assets/img/formacion.jpg', 'Formación', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, at!', '#63a7f0'),
-			new Card('/assets/img/analisis.jpg', 'Análisis', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, at!', '#cec43e'),
-			new Card('/assets/img/investigacion.jpg', 'Investigación', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, at!', '#ce3e3e'),
+			new Card('https://images.pexels.com/photos/3184465/pexels-photo-3184465.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 'Consultoría', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, at!', '#2f78c6'),
+			new Card('https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 'Formación', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, at!', '#63a7f0'),
+			new Card('https://images.pexels.com/photos/590016/pexels-photo-590016.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 'Análisis', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, at!', '#cec43e'),
+			new Card('https://images.pexels.com/photos/590022/pexels-photo-590022.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500', 'Investigación', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, at!', '#ce3e3e'),
 		];
 	}
 
